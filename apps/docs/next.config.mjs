e865cb66aed0858e758251b07631cbc810c1a178
Lam Ngoc Khuong <me@ngockhuong.com>
2025-04-20 22:25:21 +0700

    feat: upgrade docs

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,23 +1,16 @@
-/** @type {import('next').NextConfig} */
-import nextra from "nextra";
+import nextra from 'nextra';
 
 const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
+  latex: true,
+  defaultShowCopyCode: true,
+  contentDirBasePath: '/docs',
 });
 
-const isProduction = process.env.NODE_ENV === "production";
-const assetPrefix = isProduction ? "/nestjs-turbo" : "";
-
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
   reactStrictMode: true,
-  swcMinify: true,
-  assetPrefix,
-  basePath: assetPrefix,
-  output: "export",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default withNextra(nextConfig);
