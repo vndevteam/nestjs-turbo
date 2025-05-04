6ae468be65b6e47fe446ad8656aff74d88a709b1
Lâm Ngọc Khương <me@ngockhuong.com>
2025-05-04 15:04:35 +0700

    fix: add & update user logic (#150)

--- apps/realworld-graphql/src/modules/user/user.resolver.ts
+++ apps/realworld-graphql/src/modules/user/user.resolver.ts
@@ -1,12 +1,24 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { CurrentUser } from '@repo/graphql';
+import { Public } from '@repo/nest-common';
+import { AuthService } from '../auth/auth.service';
 import { CreateUserInput, UpdateUserInput } from './dto/user.dto';
 import { User } from './model/user.model';
 import { UserService } from './user.service';
 
-@Resolver()
+@Resolver(() => User)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Query(() => User)
   async currentUser(
@@ -15,13 +27,22 @@ export class UserResolver {
     return this.userService.get(user);
   }
 
+  @Public()
   @Mutation(() => User)
   async createUser(@Args('user') userInput: CreateUserInput): Promise<User> {
     return await this.userService.create(userInput);
   }
 
   @Mutation(() => User)
-  async updateUser(@Args('user') userInput: UpdateUserInput): Promise<User> {
-    return await this.userService.update(userInput);
+  async updateUser(
+    @CurrentUser('id') userId: number,
+    @Args('user') userInput: UpdateUserInput,
+  ): Promise<User> {
+    return await this.userService.update(userId, userInput);
+  }
+
+  @ResolveField()
+  async token(@Parent() user: User) {
+    return await this.authService.createToken({ id: user.id });
   }
 }
