15ebfa88f51a2b86595dccbfafe3fa312e773e52
Lam Ngoc Khuong <hi@khuong.dev>
2025-10-09 16:22:31 +0700

    feat: upgrade some deps

--- apps/docs/mdx-components.ts
+++ apps/docs/mdx-components.ts
@@ -1,8 +1,8 @@
 import { useMDXComponents as getDocsMDXComponents } from "nextra-theme-docs";
 
-const docsComponents = getDocsMDXComponents();
-
-export const useMDXComponents: typeof getDocsMDXComponents = (components) => ({
-  ...docsComponents,
-  ...components,
-});
+export function useMDXComponents(components = {}) {
+  return {
+    ...getDocsMDXComponents(components),
+    ...components,
+  };
+}
